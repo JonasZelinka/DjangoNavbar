e502577ab021f635e670b11c5102746de0f91ec6
Jonáš.Zelinka <D21787@oaopava.cz>
2024-01-23 10:19:52 +0100

    fix

--- Static1/urls.py
+++ Static1/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from Static1 import views
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("form", views.add, name="form" )
 ]
--- Static1/views.py
+++ Static1/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "Static1/index.html")
+def add(request):
+    return render(request, "Static1/form.html") 
